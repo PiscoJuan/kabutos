78adadf4a77231edc73b6d0d9f291fa2a5397397
lurapozo <lurapozo@espol.edu.ec>
2023-02-25 20:19:14 -0500

    restriccion en chat

--- src/app/chat/mensajes/mensajes.page.ts
+++ src/app/chat/mensajes/mensajes.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { IonContent, NavController } from '@ionic/angular';
+import { IonContent, NavController, ModalController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 import { interval } from 'rxjs';
 import { MensajeriaService } from 'src/app/servicios/mensajeria.service';
+import { IncorrectoPage } from '../../aviso/incorrecto/incorrecto.page';
 
 @Component({
   selector: 'app-mensajes',
@@ -12,83 +13,96 @@ import { MensajeriaService } from 'src/app/servicios/mensajeria.service';
 export class MensajesPage implements OnInit {
   @ViewChild(IonContent) content: IonContent;
   textSms = "";
-  cliente:string
+  cliente: string
   admin: any;
-  admin_seleccionado:string
+  admin_seleccionado: string
 
   interval
   constructor(
-    private navCtrl:NavController,
+    private navCtrl: NavController,
     private storage: Storage,
-    private mensajeriaService:MensajeriaService
+    public modalController: ModalController,
+    private mensajeriaService: MensajeriaService
 
-    ) {
+  ) {
 
-     }
+  }
 
   ngOnInit() {
-    this.mensajeriaService.obtenerAdmin()
-    this.storage.get("perfil").then(data=>{
-      this.cliente=data.cedula
-      this.mensajeriaService.usuario_cliente=data.cedula
+    this.storage.get("perfil").then(data => {
+      this.mensajeriaService.obtenerAdmin()
+      this.cliente = data.cedula
+      this.mensajeriaService.usuario_cliente = data.cedula
       this.mensajeriaService.obtenerListaMensajes()
       this.content.scrollToBottom(100)
 
-    })
-
-    this.interval = setInterval(() => {
-      this.mensajeriaService.obtenerListaMensajes()
-      }, 2000); 
+      this.interval = setInterval(() => {
+        this.mensajeriaService.obtenerListaMensajes()
+      }, 2000);
 
       this.interval = setInterval(() => {
         this.content.scrollToBottom(100)
-      }, 10000); 
-
+      }, 10000);
+    } )
   }
-  ionViewWillLeave(){
+  ionViewWillLeave() {
     clearInterval(this.interval);
 
-}
-  
-  ionViewDidEnter(){
-    this.mensajeriaService.obtenerAdmin()
-    this.mensajeriaService.obtenerListaMensajes()
-    this.admin_seleccionado=this.mensajeriaService.usuarios_admin[0].nombre+" "+this.mensajeriaService.usuarios_admin[0].apellido
-    this.content.scrollToBottom(100)
   }
 
-
-  
-
-  sendMessage() {
-    if (this.textSms.length > 0) {
-      console.log(this.storage.get("perfil").then(data=>{
-        data={
-          cliente:this.cliente,
-          admin:this.admin,
-          texto:this.textSms
-        }
-        console.log(data)
-        this.mensajeriaService.sendMessage(data,this.content)
-
-        this.textSms = "";
-      }))
-      
+  ionViewDidEnter() {
+    if (this.cliente) {
+      this.mensajeriaService.obtenerAdmin()
+      this.mensajeriaService.obtenerListaMensajes()
+      this.admin_seleccionado = this.mensajeriaService.usuarios_admin[0].nombre + " " + this.mensajeriaService.usuarios_admin[0].apellido
+      this.content.scrollToBottom(100)
+    }else {
+      this.mensajeIncorrecto("Perfil no encontrado","Inicie sesión para poder chatear con el administrador");
     }
   }
 
-  regresar(){
+
+  async mensajeIncorrecto(titulo:string,mensaje:string){
+    const modal = await this.modalController.create({
+      component: IncorrectoPage,
+      cssClass: 'IncorrectoProducto',
+      componentProps: {
+        'titulo': titulo,
+        'mensaje': mensaje
+        }
+      });
+      return await modal.present();
+  }
+
+  sendMessage() {
+    if (this.textSms.length > 0) {
+      console.log(this.storage.get("perfil").then(data => {
+        data = {
+          cliente: this.cliente,
+          admin: this.admin,
+          texto: this.textSms
+        }
+        console.log(data)
+        this.mensajeriaService.sendMessage(data, this.content)
+
+        this.textSms = "";
+      }))
+
+    }
+  }
+
+  regresar() {
     this.navCtrl.back()
   }
 
-  manejarAdmin(event){
+  manejarAdmin(event) {
 
-    this.admin=event.detail.value.cedula
-    this.mensajeriaService.usuario_admin=event.detail.value.cedula
+    this.admin = event.detail.value.cedula
+    this.mensajeriaService.usuario_admin = event.detail.value.cedula
     this.mensajeriaService.obtenerListaMensajes()
     this.mensajeriaService.marcar_leido()
     this.content.scrollToBottom(100)
 
   }
-  
+
 }
